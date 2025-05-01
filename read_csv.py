6a963edac5ac801e639f73078849af925f25a2f3
mihai.surubariu <mihai.surubariu@lseg.com>
2025-05-01 22:16:38 +0300

    getting each row and printing it.

--- read_csv.py
+++ read_csv.py
@@ -1,11 +1,19 @@
 import csv                     # Used to read CSV-formatted lines from the log file
 from datetime import datetime  # Used to work with timestamps
 
+tasks = {}
 
 with open("logs1.log", "r") as file:
     # Opened the file and testing I can read it.
-    print(file.read())
-    
+    # print(file.read())
+    read_logs = csv.reader(file)
+
+    #getting each row from the read logs
+    for row in read_logs:
+        #set variables for each column.
+        timestamp, task_name, status, task_id = row
+        #making sure the info prints properly
+        print(row)
 
 
 
