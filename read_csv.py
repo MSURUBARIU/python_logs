438b98d4524e0c10e85201c99692ac8516554954
mihai.surubariu <mihai.surubariu@lseg.com>
2025-05-01 22:01:01 +0300

    Initial Check-in, just adding the log file and reading it

---
+++ read_csv.py
@@ -0,0 +1,13 @@
+import csv                     # Used to read CSV-formatted lines from the log file
+from datetime import datetime  # Used to work with timestamps
+
+
+with open("logs1.log", "r") as file:
+    # Opened the file and testing I can read it.
+    print(file.read())
+    
+
+
+
+
+    
