ed88ad81f4251b17e85919f8c68cfc62cfa2ecf7
mihai.surubariu <mihai.surubariu@lseg.com>
2025-05-07 16:51:27 +0300

    Added list of started and finished tasks.

--- read_csv.py
+++ read_csv.py
@@ -4,7 +4,12 @@ import mmap
 import time
 from collections import defaultdict
 
-tasks = {}
+started_tasks = {}
+tasks = defaultdict(lambda: {"name": "", "runs": []})
+started_tasks = {}
+count_start = 0
+count_finish = 0
+count_errored = 0
 ''' ways to implement
 -1 dictionary and open(file)
 -2 for loop that searches contents every time
@@ -28,16 +33,29 @@ with open("logs1.log", "r") as logs_file:
 
         timestamp_str, task_name, status, task_id = [p.strip() for p in parts]
 
-        # for timestamp_str in parts:
-            # set variables for each column.
-            # timestamp, task_name, status, task_id = parts
-            # making sure the info prints properly
-        print(timestamp_str)
-        print(task_name)
-        print(status)
-        print(f"{task_id} \n")
+        # print(timestamp_str)
+        # print(task_name)
+        # print(status)
+        # print(f"{task_id}")
 
+        timestamp = datetime.strptime(timestamp_str, "%H:%M:%S") #converting to useable timestamp
+        # print(f"{timestamp} \n")
+        tasks[task_id]["name"] = task_name
 
+        if status == "START":
+            started_tasks[task_id] = timestamp
+            count_start=count_start+1
+
+        elif status == "END":
+            if task_id in started_tasks:
+               start_time = started_tasks.pop(task_id) #removing the task from the started list, that way we can have a task star/end multiple times
+               tasks[task_id]["runs"].append((start_time, timestamp))
+               print(f" Started and finished tasks {task_id} at {timestamp_str}")
+               count_finish=count_finish+1
+
+               
+
+print(f" started {count_start} finished {count_finish} ")
 
 
     #getting each row from the read logs
